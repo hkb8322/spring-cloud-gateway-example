334ed9282fdd3f04ceb549bb84c1f0cbe21d09cc
hkb8322 <hkb8322@naver.com>
2021-12-07 00:17:24 +0900

    #1 라우트 생성

--- src/main/java/com/meun2/scg/study/SpringCloudGatewayApplication.java
+++ src/main/java/com/meun2/scg/study/SpringCloudGatewayApplication.java
@@ -2,6 +2,9 @@ package com.meun2.scg.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
@@ -10,4 +13,8 @@ public class SpringCloudGatewayApplication {
 		SpringApplication.run(SpringCloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes().build();
+	}
 }
