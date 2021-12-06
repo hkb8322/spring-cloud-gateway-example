e89c6475c8d9e65a70473645403ae6735e31d816
hkb8322 <hkb8322@naver.com>
2021-12-07 01:22:07 +0900

    #4 FallBack 처리 추가

--- src/main/java/com/meun2/scg/study/SpringCloudGatewayApplication.java
+++ src/main/java/com/meun2/scg/study/SpringCloudGatewayApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+@RestController
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
 
@@ -22,8 +26,15 @@ public class SpringCloudGatewayApplication {
 						.uri("http://httpbin.org:80"))
 				.route(p -> p
 						.host("*.circuitbreaker.com")
-						.filters(f -> f.circuitBreaker(config -> config.setName("mycmd")))
+						.filters(f -> f.circuitBreaker(config -> config
+								.setName("mycmd")
+								.setFallbackUri("forward:/fallback")))
 						.uri("http://httpbin.org:80"))
 				.build();
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("fallback");
 	}
 }
