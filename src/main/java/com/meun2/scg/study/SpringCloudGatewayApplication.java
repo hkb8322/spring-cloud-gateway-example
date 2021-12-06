0a75ebf310796476cb04461b35c667f3de856300
hkb8322 <hkb8322@naver.com>
2021-12-06 23:52:11 +0900

    Initial commit

---
+++ src/main/java/com/meun2/scg/study/SpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.meun2.scg.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+
+}
